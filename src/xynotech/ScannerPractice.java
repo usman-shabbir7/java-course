b15512fe566cd9254af9a580ac3e2b05e933e082
usman <shabbir.usman3314@gmail.com>
2022-11-08 18:22:48 +0500

    UserInput

---
+++ src/xynotech/ScannerPractice.java
@@ -0,0 +1,10 @@
+package xynotech;
+import java.util.Scanner;
+public class ScannerPractice {
+    public static void main(String[] args) {
+        System.out.print("enter number : ");
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        System.out.println("You enter "+ a);
+    }
+}
