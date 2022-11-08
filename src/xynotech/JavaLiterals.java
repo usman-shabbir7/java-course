344425137277fa172dd50b7e9d07985a909963ac
usman <shabbir.usman3314@gmail.com>
2022-11-08 18:08:08 +0500

    veriable_litrals

---
+++ src/xynotech/JavaLiterals.java
@@ -0,0 +1,19 @@
+package xynotech;
+
+public class JavaLiterals {
+    public static void main(String[] args) {
+        byte age=87;
+        short d=150;
+        int salary=50000;
+        float f1=4.5f;
+        double d1=5.6;
+        double d2=5.6d;
+        char a='A';
+        long memory=2473673543437l;
+        boolean m=true;
+        System.out.println(salary);
+        String name="Usman";
+        System.out.println(name);
+
+    }
+}
